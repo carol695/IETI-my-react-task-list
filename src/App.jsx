ab6eef3989681f52bf2dc6265473a17fc6161b10
carol <caroltatianacely@gmail.com>
2023-10-25 18:53:28 -0500

    project-6

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import Header from "./components/Header"
 import { TaskList } from "./components/TaskList";
 import React, {useState, useEffect} from "react";
 import { useCreateTask } from "./hooks/useTaskList";
-import Modal from "react-modal";
+import { Modal, Button, Flex, ModalOverlay, ModalContent, ModalHeader, ModalBody, ModalFooter, FormControl, FormLabel, Input, FormHelperText } from "@chakra-ui/react";
+import { AddIcon,DeleteIcon } from "@chakra-ui/icons";
+import { useModalStyles } from "@chakra-ui/react";
 
 
 function App() {
@@ -14,6 +16,7 @@ function App() {
     task:undefined,
     description:undefined,
   })
+  
 
   function handleChange(event) {
     const val = event.target.value;
@@ -63,49 +66,36 @@ function App() {
 
   const isValid = Object.keys(formValidation).every(key=>formValidation[key]==="")
 
-  return (
-    <div className="App">
+  return(
+    <Flex alignItems="center" justifyContent="center" minHeight="auto" flexDirection="column" className="App">
       <Header />
-      <button onClick={handleOpenModal}>Agregar Tarea</button>
+      <Button mb={7} onClick={handleOpenModal} colorScheme="teal" variant="outline" leftIcon={<AddIcon/>}>Agregar</Button>
       <Modal isOpen={isOpen} onRequestClose={handleCloseModal}>
-        <form onSubmit={handleSubmit} className="task-form">
-          <div className="form-group">
-            <label htmlFor="taskName">Nombre de la Tarea</label>
-            <input
-              type="text"
-              id="taskName"
-              value={value}
-              onChange={handleChange}
-              className="form-control"
-              placeholder="Escribe el nombre de la tarea"
-              required
-            />
-            <span className="error-message">{formValidation.task}</span>
-          </div>
-          <div className="form-group">
-            <label htmlFor="taskDescription">Descripción de la Tarea</label>
-            <input
-              type="text"
-              id="taskDescription"
-              value={description}
-              onChange={handleDescriptionChange}
-              className="form-control"
-              placeholder="Escribe la descripción de la tarea"
-              required
-            />
-            <span className="error-message">{formValidation.description}</span>
-          </div>
-          <button disabled={!isValid} type="submit" className="btn btn-primary">
-            Agregar Tarea
-          </button>
-        </form>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Agregar Tarea</ModalHeader>
+          <ModalBody>
+            <FormControl isInvalid={formValidation.task}>
+              <FormLabel>Tarea</FormLabel>
+              <Input value = {value} onChange={handleChange}/>
+              <FormHelperText color={"red"}>{formValidation.task}</FormHelperText>
+            </FormControl>
+            <FormControl>
+              <FormLabel>Descripcion</FormLabel>
+              <Input value = {description} onChange={handleDescriptionChange}/>
+              <FormHelperText color={"red"}>{formValidation.description}</FormHelperText>
+            </FormControl>
+          </ModalBody>
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={handleCloseModal}> Cancelar </Button>
+            <Button colorScheme="green" onClick={handleSubmit} isDisabled={!isValid}>Agregar</Button>
+          </ModalFooter>
+        </ModalContent>
       </Modal>
-  
-      <TaskList list={items} onDeleteTask={handleDeleteItem} />
-      <button onClick={handleDeleteAll} className="btn btn-danger">
-        Eliminar Todas las Tareas
-      </button>
-    </div>
+      
+      <TaskList list={items} onDeleteTask={handleDeleteItem}/>
+      <Button onClick={handleDeleteAll} colorScheme="teal" leftIcon={<DeleteIcon/>} mt={7}>Eliminar todos</Button>
+    </Flex>
   );
 }
 
